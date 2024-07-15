2751a35f27068558826810a0a0e7d4094b931e9b
kimcorneil <kcorneil3713@gmail.com>
2024-07-15 11:23:20 -0400

    Master plan created

--- Assignment4.R
+++ Assignment4.R
@@ -3,3 +3,13 @@
 # July 19, 2024
 
 ####### MASTER PLAN #######
+#' Save file into Assignment4 folder
+#' Set working directory to Assignment4 (informing user they will need to ensure the file is in their wd)
+#' Read the data into a dataframe 
+#' View and summarize the data
+#' Clean the data and ensure it is in the correct format
+#' Remove potential hoaxs
+#' Add report_delay
+#' Remove sightings that were reported before they happened
+#' Create average report_delay table per country
+#' Create a histogram of the duration per seconds column
