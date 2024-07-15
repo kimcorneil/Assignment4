0f5adf442d3634330d0c89bed223574a3a2c3c2a
kimcorneil <kcorneil3713@gmail.com>
2024-07-15 16:13:50 -0400

    Fixing some dates and times

--- Assignment4.R
+++ Assignment4.R
@@ -31,4 +31,36 @@ names(ufos)
 #' some countries missing
 #' zeros in duration.seconds
 
+# Fix date issue in datetime and date_posted
+# Install tidyr - previously installed so add to library 
+library("tidyr")
+# Make datetime into seperate columns for dates and times observed
+ufos2 <- separate(ufos, datetime, into = c("date_observed", "time_observed"), sep = " ")
+# View and summarize the data after making changes
+View(ufos2)
+summary(ufos2)
+names(ufos2)
+
+# Make date_observed and date_posted into recognizable dates
+# Install lubridate - previously installed so add to library 
+library("lubridate")
+# duplicate ufos2 and name it ufos3
+ufos3 <- ufos2
+# convert date_observed to ymd format so it can be recognized as a date and not a character
+ufos3$date_observed <- ymd(ufos3$date_observed)
+# convert date_posted to dmy format so it can be recognized as a date and not a character
+ufos3$date_posted <- dmy(ufos3$date_posted)
+# convert date_posted to ymd so it is presented in the same format as date_observed for consistency
+ufos3$date_posted <- ymd(ufos3$date_posted)
+# View and summarize the data after making changes
+View(ufos3)
+summary(ufos3)
+
+# duplicate ufos3 and name it ufos4
+ufos4 <- ufos3
+# Make time_observed into a time period using lubridate and not a character
+ufos4$time_observed <- hm(ufos4$time_observed)
+# View and summarize the data after making changes
+View(ufos4)
+summary(ufos4)
 
